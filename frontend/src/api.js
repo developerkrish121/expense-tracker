2a2462729a362c4df46d441413bde4ef370cb057
Krishna Bansal <krishnabansal27august2005@gmail.com>
2026-05-01 13:36:16 +0530

    fix netlify routing

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5004"
+  baseURL: "https://expense-tracker-y6hk.onrender.com"
 });
 
 // 🔥 Attach token automatically to every request
